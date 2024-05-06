0c03df35d5b10b9d7953e74b202a7ab31d689e5e
mfinette <117938455+mfinette@users.noreply.github.com>
2024-05-07 00:16:19 +0200

    changes

--- main.js
+++ main.js
@@ -19,14 +19,34 @@ const scene = new THREE.Scene();
 // Set the background color to black
 scene.background = new THREE.Color('black');
 
+// Make the background be a picture inside of a sphere
+// Create a sphere geometry
+const sphereGeometry = new THREE.SphereGeometry(200, 32, 32);
+
+// Load the background texture
+const textureLoader = new THREE.TextureLoader();
+const backgroundTexture = textureLoader.load('background4.jpeg');
+
+// Create a material using the background texture
+const backgroundMaterial = new THREE.MeshBasicMaterial({
+    map: backgroundTexture,
+    side: THREE.BackSide, // Render the texture on the inside of the sphere
+});
+
+// Create a sphere mesh
+const sphere = new THREE.Mesh(sphereGeometry, backgroundMaterial);
+
+// Add the sphere to the scene
+scene.add(sphere);
+
 // Create a box geometry and material
 const geometry = new THREE.BoxGeometry(9, 16, 9);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
 const video = document.getElementById('video');
-video.onloadeddata = function () {
-    video.play();
-};
+//video.onloadeddata = function () {
+  //  video.play();
+//};
 
 // Create your video texture
 const videoTexture = new THREE.VideoTexture(video);
@@ -54,14 +74,18 @@ scene.add(box);
 // Create a perspective camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5; // Initial distance from the box
-camera.position.y = 20; // Initial height from the box
+camera.position.y = 17; // Initial height from the box
 
 // Add ambient light
 const light = new THREE.AmbientLight(0xffffff, 0.5);
-// scene.add(light);
+const cameraLight = new THREE.PointLight(0xffffff, 100);
+scene.add(light);
 // Add grid helper
-const gridHelper = new THREE.GridHelper(100, 100, 0xfc03e8, 0xfc03e8);
+const gridHelper = new THREE.GridHelper(1000, 1000, 0xfc03e8, 0xfc03e8);
 scene.add(gridHelper);
+
+// fog
+scene.fog = new THREE.FogExp2(0x000000, 0.005);
 
 // Create a WebGL renderer
 const renderer = new THREE.WebGLRenderer();
@@ -104,10 +128,12 @@ bandPassFilter.gain.value = 6; // Adjust the gain
 analyser.connect(bandPassFilter);
 
 
+const slayText = document.getElementById('slayText');
+
 // Function to load and play the audio
 function loadAndPlayAudio() {
     audioContext.resume().then(function() {
-        audioLoader.load('bangarang.mp3', function(buffer) {
+        audioLoader.load('anniv.mp3', function(buffer) {
             // Create an AudioBufferSourceNode
             var source = audioContext.createBufferSource();
             source.buffer = buffer;
@@ -120,59 +146,105 @@ function loadAndPlayAudio() {
     });
 }
 
+let hasbeenclicked = false;
 // Event listener to start playing the audio when the document is clicked
 document.addEventListener('click', function() {
-    loadAndPlayAudio();
+    if (!hasbeenclicked)
+        {
+            hasbeenclicked = true;
+            loadAndPlayAudio();
+            startAnimation();
+            slayText.style.display = 'none';
+        }
 });
 
-// Function to scale the box according to the music volume
-function scaleBoxAccordingToMusicVolume(analyser, box) {
-    // Get the frequency data
-    const frequencyData = new Uint8Array(analyser.frequencyBinCount);
-    analyser.getByteFrequencyData(frequencyData);
-
-    // Calculate the average volume
-    let totalVolume = 0;
-    for (let i = 0; i < frequencyData.length; i++) {
-        totalVolume += frequencyData[i];
+class Radius {
+    constructor(value) {
+        this.radius = value;
+        this.increasing = true;
+        this.decreasing = false;
+        this.orbitSpeed = 0.0033;
+        this.cameraHeight = 60;
+        this.cameraAnimating = false;
     }
-    const averageVolume = totalVolume / frequencyData.length;
-
-    // Calculate the scale factor based on the average volume
-    let scaleFactor = (averageVolume / 255) + 0.1; // Assuming the volume data is normalized between 0 and 255
-    // Create a Tween to smoothly scale the box
-    const tween = new TWEEN.Tween(box.scale)
-    .to({ x: scaleFactor * 5, y: scaleFactor * 5, z: scaleFactor * 5}, 100) // Duration of the animation
-    .easing(TWEEN.Easing.Quadratic.Out) // Easing function
-    tween.start();
 }
 
-// Assuming you have a box mesh in your scene
-// Call the scaling function periodically to update the box's scale based on the audio volume
-setInterval(function() {
-    scaleBoxAccordingToMusicVolume(analyser, box);
-}, 100); // Adjust the interval as needed
+let radiusObject = new Radius(15);
+
+function changeRadius(radius)
+{
+    const duration = 1500;
+    const increase = 15;
+    if (radius.decreasing)
+    {
+        const target = radius.radius - increase;
+        radius.decreasing = false;
+        new TWEEN.Tween(radius)
+        .to({radius: target}, duration)
+        .onComplete(() => {
+            radius.increasing = true;
+        })
+        .easing(TWEEN.Easing.Linear.None)
+        .start();
+    }
+    if (radius.increasing)
+        {
+            const target = radius.radius + increase;
+            radius.increasing = false;
+            new TWEEN.Tween(radius)
+            .to({radius: target}, duration)
+            .onComplete(() => {
+                radius.decreasing = true;
+            })
+            .easing(TWEEN.Easing.Linear.None)
+            .start();
+        }
+}
+
+function startAnimation()
+{
+    if (!radiusObject.cameraAnimating)
+    {
+        const duration = 4000;
+        radiusObject.cameraAnimating = true;
+        new TWEEN.Tween(radiusObject)
+        .to({cameraHeight: 17}, duration)
+        .easing(TWEEN.Easing.Linear.None)
+        .start();
+        new TWEEN.Tween(radiusObject)
+        .to({orbitSpeed: 0.0003}, duration)
+        .easing(TWEEN.Easing.Linear.None)
+        .onUpdate(() => {
+        })
+        .onComplete(() => {
+            video.play();
+        })
+        .start();
+    }
+}
 
 // Function to update camera position and look at the box
 function updateCamera() {
-    const radius = 55; // Distance from the box
-    const elapsedTime = performance.now() * 0.0005; // Speed of orbit
+    const elapsedTime = performance.now() * radiusObject.orbitSpeed; // Speed of orbit
 
     // Update camera position
-    camera.position.x = radius * Math.cos(elapsedTime);
-    camera.position.z = radius * Math.sin(elapsedTime);
+    camera.position.x = radiusObject.radius * Math.cos(elapsedTime);
+    camera.position.z = radiusObject.radius * Math.sin(elapsedTime);
 
     // Make sure the camera is always looking at the box
     camera.lookAt(box.position);
+    camera.position.y = radiusObject.cameraHeight;
+    //cameraLight.position.copy(camera.position);
 }
 
 // Animation loop
 function animate() {
     requestAnimationFrame(animate);
     updateCamera();
+    changeRadius(radiusObject);
     TWEEN.update();
-    // renderer.render(scene, camera);
-    composer.render();
+    renderer.render(scene, camera);
+    //composer.render();
 }
 
 animate();
